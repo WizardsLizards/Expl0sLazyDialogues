5be779c87f6d5ba6aafd47148c822d15fb73d01d
WizardsLizards <focdom@gmail.com>
2021-10-22 01:53:43 +0200

    Initial commit
    Dialogue-Dialogues work,
    question dialogue-display works.
    
    TODO:
    string-dialogues & list<string>-dialogues (display not working),
    question dialogue (buttons not implemented)

---
+++ Expl0sLazyDialogues/Expl0sLazyDialogues/ModEntry.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace Expl0sLazyDialogues
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="IHelper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper IHelper)
+        {
+            #region setup
+            var csHarmony = new Harmony(this.ModManifest.UniqueID);
+
+            ObjectPatches.Initialize(this.Monitor, IHelper);
+            #endregion
+
+            #region Harmony patches
+            //Patches the keyboard input, to receive 1-0 and space
+            csHarmony.Patch(
+               original: AccessTools.Method(typeof(StardewValley.Menus.DialogueBox), "receiveKeyPress"),
+               prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.receiveKeyPress_Prefix))
+            );
+
+            //multiple constructors for messageboxes, questionboxes, etc.
+            csHarmony.Patch(
+               original: AccessTools.Constructor(typeof(StardewValley.Menus.DialogueBox), new Type[] { typeof(string) }),
+               prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.DialogeBox_String_Prefix))
+            );
+
+            csHarmony.Patch(
+               original: AccessTools.Constructor(typeof(StardewValley.Menus.DialogueBox), new Type[] { typeof(List<string>) }),
+               prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.DialogeBox_List_Prefix))
+            );
+
+            csHarmony.Patch(
+               original: AccessTools.Constructor(typeof(StardewValley.Menus.DialogueBox), new Type[] { typeof(Dialogue) }),
+               prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.DialogeBox_Dialogue_Prefix))
+            );
+
+            csHarmony.Patch(
+               original: AccessTools.Constructor(typeof(StardewValley.Menus.DialogueBox), new Type[] { typeof(string), typeof(List<Response>), typeof(int) }),
+               prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.DialogeBox_Question_Prefix))
+            );
+            #endregion
+        }
+    }
+}
---
+++ Expl0sLazyDialogues/Expl0sLazyDialogues/ObjectPatches.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Menus;
+using StardewValley.Monsters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expl0sLazyDialogues
+{
+    class ObjectPatches
+    {
+        internal static IMonitor IMonitor;
+        internal static IModHelper IHelper;
+
+        internal static void Initialize(IMonitor IMonitor, IModHelper IHelper)
+        {
+            ObjectPatches.IMonitor = IMonitor;
+            ObjectPatches.IHelper = IHelper;
+        }
+
+        internal static bool receiveKeyPress_Prefix(DialogueBox __instance, Keys key)
+        {
+            if (key == Keys.Space)
+            {
+                __instance.receiveLeftClick(0, 0);
+                return false;
+            }
+
+            return true;
+        }
+
+        internal static bool DialogeBox_String_Prefix(string dialogue)
+        {
+            Game1.addHUDMessage(new HUDMessage("string"));
+
+            dialogue += "^^> Press Space to continue...";
+
+            return true;
+        }
+
+        internal static bool DialogeBox_List_Prefix(List<string> dialogues)
+        {
+            Game1.addHUDMessage(new HUDMessage("List<string>"));
+
+            for (int iCounter = 0; iCounter < dialogues.Count; iCounter++)
+            {
+                dialogues[iCounter] += "^^> Press Space to continue...";
+            }
+
+            return true;
+        }
+
+        internal static bool DialogeBox_Dialogue_Prefix(Dialogue dialogue)
+        {
+            Game1.addHUDMessage(new HUDMessage("Dialogue"));
+
+            for (int iCounter = 0; iCounter < dialogue.dialogues.Count; iCounter++)
+            {
+                string temp = dialogue.dialogues[iCounter];
+                string addOn = "^^> Press Space to continue...";
+                string subTemp = temp.Substring(temp.Length - addOn.Length);
+                if (subTemp.Equals(addOn) == false)
+                {
+                    dialogue.dialogues[iCounter] += addOn;
+                }
+            }
+            
+            return true;
+        }
+
+        internal static bool DialogeBox_Question_Prefix(string dialogue, List<Response> responses, int width)
+        {
+            Game1.addHUDMessage(new HUDMessage("Question"));
+
+            for (int iCounter = 0; iCounter < responses.Count; iCounter++)
+            {
+                responses[iCounter].responseText = (iCounter + 1).ToString() + ". " + responses[iCounter].responseText;
+            }
+
+            return true;
+        }
+    }
+}
